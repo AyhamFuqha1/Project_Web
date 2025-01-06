08ee4b7cdd392c69e1fb8caba4ac7e5c8d999189
Taima-Turabe <151634510+Taima-Turabe@users.noreply.github.com>
2025-01-07 01:26:44 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,27 +1,27 @@
 <header>
 
-    <div class="header">
-        <div class="log">
-          <img src="assets/img/Template_1/logo.svg">
-        </div>
-        
-        <div class="nav" class="sidebar-item categories">
-          <ul> 
-            <li><a href="home.html" style="color:rgb(21, 146, 125)">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="register.php">Register</a></li>
-            <li><a href="#">Blog</a></li>
-          </ul>
-          <div class="clear"></div>
-        </div>
-      
-        <div class="but">
-          <button>LogeIn</button>
-        </div>
-      
-      </div>
+  <div class="header" style="height: 74px;">
+    <div class="log">
+      <img style="width: 30%; margin-left: 10%; margin-top: 16px;" src="assets/img/Template_1/logo.svg">
+    </div>
+
+    <div class="nav" class="sidebar-item categories">
+      <ul>
+        <li><a href="home.html" style="color:rgb(21, 146, 125)">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="#">Blog</a></li>
+      </ul>
+      <div class="clear"></div>
+    </div>
+
+    <div class="but">
+      <button>Loge</button>
+    </div>
+
+  </div>
 
 
 
